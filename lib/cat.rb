3dade11f070ec8c8b1619c9bf1ef332f9d27408f
Trac Nguyen <nguyendtrac@gmail.com>
2018-04-08 17:40:55 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,11 @@
 class Cat
-  attr_accessor :mood
+
   attr_reader :name
+  attr_accessor :mood
 
   def initialize(name)
     @name = name
     @mood = "nervous"
   end
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,25 +1,25 @@
 class Owner
+  attr_accessor :pets, :name
   attr_reader :species
-  attr_accessor :name, :pets
 
-  @@all = []
-
-  def initialize(species)
-    @species = species
-    @@all << self
-    @pets = {fishes: [], cats: [], dogs: []}
-  end
+  OWNERS = []
 
   def self.all
-    @@all
+    OWNERS
   end
 
   def self.count
-    self.all.count
+    OWNERS.size
   end
 
   def self.reset_all
-    @@all.clear
+    OWNERS.clear
+  end
+
+  def initialize(species)
+    @species = species
+    @name = name
+    @pets = {fishes: [], cats: [], dogs: []}
   end
 
   def say_species
@@ -47,21 +47,21 @@ class Owner
     end
   end
 
-  def play_with_cats
-    self.pets[:cats].collect do |cat|
-      cat.mood = "happy"
-    end
-  end
-
   def feed_fish
     self.pets[:fishes].collect do |fish|
       fish.mood = "happy"
     end
   end
 
+  def play_with_cats
+    self.pets[:cats].collect do |cat|
+      cat.mood = "happy"
+    end
+  end
+
   def sell_pets
-    new_array = self.pets.values.flatten
-    new_array.collect do |pet|
+    sold = self.pets.values.flatten
+    sold.collect do |pet|
       pet.mood = "nervous"
     end
     self.pets.clear
@@ -70,4 +70,5 @@ class Owner
   def list_pets
     "I have #{self.pets[:fishes].count} fish, #{self.pets[:dogs].count} dog(s), and #{self.pets[:cats].count} cat(s)."
   end
+
 end
